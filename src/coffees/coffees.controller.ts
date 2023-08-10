2b12cb0d83b7b066aad01c5ceb891ae700123c25
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2023-08-10 20:40:03 +0100

    feat: crud with controller and service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,17 +1,31 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Query, HttpCode, HttpStatus, Patch, Delete } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {
+
+  }   
   @Get()
-  findAll() {
-    return `this action returns all coffees`;
+  findAll(@Query() paginationQuery) {
+    // const { limit, offset } = paginationQuery;
+    return this.coffeeService.findAll();
   }
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string) {
-    return `this action returns #${id} coffee`;
+    return this.coffeeService.findOne(id);
   }
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coffeeService.create(body);
+  }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return this.coffeeService.update(id, body);
+  }
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.coffeeService.remove(id);
   }
 }
